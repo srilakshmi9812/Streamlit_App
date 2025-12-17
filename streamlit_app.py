842996444ff6401d825efd6965509ca3a6c30f4f
srilakshmi9812 <srilakshmimanduri5@gmail.com>
2025-12-17 00:54:29 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,23 +92,23 @@ st.dataframe(df_prediction_proba,
                'Adelie': st.column_config.ProgressColumn(
                  'Adelie',
                  format='%f',
-                 width='medium'
-                 min_value=0
-                 max_value=1
+                 width='medium',
+                 min_value=0,
+                 max_value=1,
                  ),
                 'Chinstrap': st.column_config.ProgressColumn(
                  'Chinstrap',
                  format='%f',
-                 width='medium'
-                 min_value=0
-                 max_value=1
+                 width='medium',
+                 min_value=0,
+                 max_value=1,
                  ),
                 'Gentoo': st.column_config.ProgressColumn(
                  'Gentoo',
                  format='%f',
-                 width='medium'
-                 min_value=0
-                 max_value=1
+                 width='medium',
+                 min_value=0,
+                 max_value=1,
                  )}, hide_index=True)
                  
 penguin_species = np.array(['Adelie', 'Chinstrap', 'Gentoo'])
