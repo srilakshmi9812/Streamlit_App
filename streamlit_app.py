3e3f93ced9fa7d78f4a408d7afa964530c8f1f75
srilakshmi9812 <srilakshmimanduri5@gmail.com>
2025-12-16 23:53:37 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,15 +13,30 @@ with st.expander("Data"):
   st.write("**Raw Data**")
   data = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv")
   data
-
-st.write("### **Input Features**")
-X= data.drop("species", axis = 1)
-X
-
-st.write("### **Output, Y**")
-Y = data.species
-Y
-
+  
+  st.write("### **Input Features**")
+  X= data.drop("species", axis = 1)
+  X
+  
+  st.write("### **Output, Y**")
+  Y = data.species
+  Y
+  
 with st.expander("Data Visualization"):
   st.scatter_chart(data=data, x="bill_length_mm", y="body_mass_g", color = 'species')
+
+#Data Preparation:
+
+with st.sidebar:
+  st.header("Input Features")
+  island = st.selectbox("Island", ['Biscoe', 'Dream', 'Torgersen'])
+  gender = st.selectbox("Gender", ['male', 'female'])
+  bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
+  bill_depth_mm = st.slider('Bill depth (mm)', 13.1 21.5, 17.2)
+  flipper_length_mm = st.slider('flipper length (mm)', 172.0, 231.0, 201.0)
+  body_mass_g = st.slider('body mass (g)', 2600.0, 6300.0, 4207.0)
+
+                  
+                        
+  
 
