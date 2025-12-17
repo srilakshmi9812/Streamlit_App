a8b9c349d51b2efa3978a90fb17d6b64a273907f
srilakshmi9812 <srilakshmimanduri5@gmail.com>
2025-12-17 01:06:23 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,7 +83,8 @@ clf.fit(X_encode, y_encode)
 prediction = clf.predict(input_row)
 prediction_proba = clf.predict_proba(input_row)
 df_prediction_proba = pd.DataFrame(prediction_proba)
-df_prediction_proba.rename(columns = {0: 'Adelie', 1: 'Chinstrap', 2: 'Gentoo'})
+#df_prediction_proba.rename(columns = {0: 'Adelie', 1: 'Chinstrap', 2: 'Gentoo'})
+df_prediction_proba.columns = ['Adelie', 'Chinstrap', 'Gentoo']
 df_prediction_proba
 
 #Display Predicted Species
