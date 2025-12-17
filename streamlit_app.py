fd7fbd17cff118aaf4e18846120697781b786328
srilakshmi9812 <srilakshmimanduri5@gmail.com>
2025-12-17 00:01:05 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,8 @@ data = {'island': island,
         'body_mass_g': body_mass_g,
         'gender': gender}
 input_df = pd.DataFrame(data, index=[0])
-input_df
+input_penguins = pd.concat([input_df, X], axis = 0)
+input_penguins
 
                   
                         
