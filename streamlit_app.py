745670e6e2590ec7b264f83bdbd85cbb8cc74463
srilakshmi9812 <srilakshmimanduri5@gmail.com>
2025-12-17 00:49:11 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -82,7 +82,37 @@ clf.fit(X_encode, y_encode)
 #Apply the model
 prediction = clf.predict(input_row)
 prediction_proba = clf.predict_proba(input_row)
+df_prediction_proba = pd.DataFrame(prediction_proba)
+df_preciction_proba.rename(columns = ['Adelie', 'Chinstrap', 'Gentoo'])
 
+#Display Predicted Species
+st.subheader('Predicted Species')
+st.dataframe(df_prediction_proba, 
+             column_config = {
+               'Adelie': st.column_config.ProgressColumn(
+                 'Adelie',
+                 format='%f',
+                 width='medium'
+                 min_value=0
+                 max_value=1
+                 ),
+                'Chinstrap': st.column_config.ProgressColumn(
+                 'Chinstrap',
+                 format='%f',
+                 width='medium'
+                 min_value=0
+                 max_value=1
+                 ),
+                'Gentoo': st.column_config.ProgressColumn(
+                 'Gentoo',
+                 format='%f',
+                 width='medium'
+                 min_value=0
+                 max_value=1
+                 )}, hide_index=True)
+                 
+penguin_species = np.array(['Adelie', 'Chinstrap', 'Gentoo'])
+st.success(str[penguins_species[prediction][0])))
   
 
 
