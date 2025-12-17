286f042aee0904bb7d4763648c2f9b1b28e2c5e5
srilakshmi9812 <srilakshmimanduri5@gmail.com>
2025-12-17 01:02:11 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -113,7 +113,7 @@ st.dataframe(df_prediction_proba,
                  )}, hide_index=True)
                  
 penguin_species = np.array(['Adelie', 'Chinstrap', 'Gentoo'])
-st.success(str[penguin_species[prediction][1]])
+st.success(str(penguin_species[prediction][0]))
   
 
 
